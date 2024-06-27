6d8a5a4157609fb0d93938a5ab69f51ec21a610e
unknown <jihadhijazi977@gmail.com>
2024-06-28 02:46:29 +0300

    Navbar, tasks and statuses container done

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,7 +1,14 @@
-let side_bar = document.getElementsByClassName("side-bar")
 
+let side_bar = document.getElementsByClassName("side-bar")
 
 let btn = document.querySelector('.open-side-bar')
 btn.addEventListener('click', function(){
 side_bar[0].classList.toggle("show-hide")
 })
+
+
+
+const now = new Date();
+
+const currentDateTime = now.toLocaleString();
+document.getElementById('time').innerText = currentDateTime
